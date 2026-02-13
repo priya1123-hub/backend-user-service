c996e75749486640dc3e0d0cf7a1eee23d662f72
priya <priyatimcy1123@gmail.com>
2026-02-13 12:34:58 +0530

    Update database connection configuration
    Updated database connection details in db.js.

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-module.exports = {
-  connect: () => {
-    console.log("User Service DB connected");
-  }
-};
+DB_HOST=database-1.caro46y6mc1q.us-east-1.rds.amazonaws.com
+DB_USER=admin
+DB_PASSWORD=ST8edgq8pZMrXaBOdln9
+DB_NAME=database-1
+DB_PORT=3306

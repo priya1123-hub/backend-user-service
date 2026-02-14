2187a9dba6ab18c784324646c13b6458346df375
priya <priyatimcy1123@gmail.com>
2026-02-14 17:55:32 +0530

    Add CORS support and update user name

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-
 const express = require("express");
+const cors = require("cors");
+ 
 const app = express();
-
 const PORT = process.env.PORT || 4000;
-
+ 
+app.use(cors({
+  origin: "https://timcy.shop", // replace with your real domain
+}));
+ 
 app.get("/users", (req, res) => {
   res.json([
-    { id: 1, name: "priya" },
+    { id: 1, name: "Siva" },
     { id: 2, name: "DevOps User" }
   ]);
 });
-
+ 
 app.get("/health", (req, res) => {
   res.send("User Service is healthy");
 });
-
-app.listen(PORT, () =>
-  console.log(`User Service running on port ${PORT}`)
-);
+ 
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`User Service running on port ${PORT}`);
+});
